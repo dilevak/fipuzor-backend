9717495d678fb2ca0bde333e665158e5b91656c3
dilevak <dilevak@student.unipu.hr>
2023-08-16 23:21:55 +0200

    Baza za kartice, slanje userID-a iz baze frontu

--- src/authRoutes.js
+++ src/authRoutes.js
@@ -1,16 +1,73 @@
 import express from 'express';
 import connect from './db.js';
 import { createCard } from './cardController.js';
-//Bcrypt library ya kriptiranje passworda
+//Bcrypt library za kriptiranje passworda
 import bcrypt from 'bcrypt';
 
 
 const router = express.Router();
 
 //AddCard ruta
-router.post('/addcard', (req, res) => {
-  console.log('Received a request to add a card:', req.body); // Log the request body
-  createCard(req, res); // Delegate the request handling to your createCard function
+router.post('/cards', async (req, res) => {
+  const { userID, name, cardNumber, expireDate, logo } = req.body;
+
+  try {
+    console.log('Received request to add card:', req.body);
+
+    const client = await connect();
+    const db = client.db('fipuzor');
+
+    //Provjera ako "cards" collection postoji
+    const collections = await db.listCollections({ name: 'cards' }).toArray();
+    if (collections.length === 0) {
+      await db.createCollection('cards');
+    }
+
+    const cardsCollection = db.collection('cards');
+
+    const newCard = {
+      userID,
+      name,
+      cardNumber,
+      expireDate,
+      logo,
+    };
+
+    const result = await cardsCollection.insertOne(newCard);
+    client.close();
+
+    if (result.insertedCount === 1) {
+      console.log('Card added successfully:', result.insertedId);
+      res.json({ success: true, message: 'Card added successfully' });
+    } else {
+      console.log('Card added but response marked as failed');
+      res.json({ success: true, message: 'Card added but response marked as failed' });
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).json({ success: false, message: 'An error occurred while connecting', error: error.message });
+  }
+});
+
+//Ruta za dohvacanje kartica po userID-u
+
+router.get('/cards/:userID', async (req, res) => {
+  const userID = req.params.userID;
+
+  try {
+    const client = await connect();
+    const db = client.db('fipuzor');
+    const cardsCollection = db.collection('cards');
+
+    // Find all cards associated with the userID
+    const cards = await cardsCollection.find({ userID }).toArray();
+    client.close();
+
+    res.json({ success: true, cards });
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).json({ success: false, message: 'An error occurred while connecting' });
+  }
 });
 
 //Login ruta
@@ -31,7 +88,7 @@ router.post('/login', async (req, res) => {
       const isPasswordValid = await bcrypt.compare(password, user.password);
       if (isPasswordValid) {
         console.log('Login successful for:', username);
-        res.json({ success: true, username: user.username }); //dodavanje username-a u odgovor
+        res.json({ success: true, userID: user._id, username: user.username });
       } else {
         console.log('Login failed for:', username);
         res.json({ success: false, message: 'Invalid credentials' });
